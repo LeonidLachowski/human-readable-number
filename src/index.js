b85a22bd7f3e44650aaedcc3c68e159098aee557
Maksim Liakhouski <lyakhovskiym@gmail.com>
2023-02-27 23:22:50 +0100

    toReadable function implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function toReadable (number) {
-  
+    const first = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+    const tens = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+    const mad = ['', 'thousand', 'million', 'billion', 'trillion'];
+    let word = '';
+    number = number + '';
+    console.log(number);
+    const length = number.length;
+    const digits = number[length - 1];
+    if(length == 1) {
+        if(parseInt(digits) == 0) word = 'zero'
+        else word = first[parseInt(digits)]
+    } else {
+        if(length == 3) word = first[parseInt(number[0])] + 'hundred ';
+        if(parseInt(number[length-2] + digits) >= 20) word += tens[parseInt(number[length-2])] + ' ' + first[parseInt(digits)];
+        else word += first[parseInt(number[length-2] + digits)];
+    }
+    return word.trim();  
 }
